9f9d70e8d390d0d000616bd6057f27cc7147f682
APGSwizzVR <randommonke71@gmail.com>
2026-09-20 01:46:10 +0100

    Rebuild Atlas FS as a finished cinematic flight simulator product site

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.ATLAS_CONFIG={DISCORD_URL:"",COFFEE_URL:"",SOCIALS:{youtube:"",tiktok:"",instagram:"",x:""}};
+window.ATLAS_CONFIG={DISCORD_URL:"",YOUTUBE_URL:"",TIKTOK_URL:"",INSTAGRAM_URL:"",X_URL:"",COFFEE_URL:""};
